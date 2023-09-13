b3b96aa5f5b657ce0487c5add845045c2b0f5aa2
DatVuongTrong <trongdatvuong@gmail.com>
2023-09-12 21:52:54 -0400

    add env file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	"github.com/DatVuongTrong/simple_bank/api"
+	db "github.com/DatVuongTrong/simple_bank/db/sqlc"
+	util "github.com/DatVuongTrong/simple_bank/db/utils"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+
+	if err != nil {
+		log.Fatal("Cannot start server")
+	}
+}
